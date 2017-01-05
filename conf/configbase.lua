1d7c3a1f2f17c7be0a549def9dd645109914f781
qtj <tianjieqin@126.com>
2017-01-05 19:32:45 +0800

    temp update

--- conf/configbase.lua
+++ conf/configbase.lua
@@ -4,4 +4,11 @@ _M._VERSION = '1.0.0'
 
 _M.kk="base"
 
+-- redis 相关配置
+_M.redis_host="192.168.142.128:6378:123456,192.168.142.128:6379:123456" -- redis host 信息:  ip:port:auth 格式
+_M.redis_dbid=0
+_M.redis_poolsize=100
+_M.redis_idletime=50000
+_M.redis_timeout=500000
+
 return _M
